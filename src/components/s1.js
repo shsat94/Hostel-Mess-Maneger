3cb8873aa5352867a04bcf5168dac02a50474e37
shsat94 <shsat94@gmail.com>
2024-08-16 17:19:44 +0530

    16-08-2024 - profile router created

---
+++ src/components/s1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function s1() {
+  return (
+    <div>
+      <p>hello 1 </p>
+      <h1>hellooooo</h1>
+    </div>
+  )
+}
---
+++ src/components/s2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function s2() {
+  return (
+    <div>
+      <h1>hello </h1>
+    </div>
+  )
+}
